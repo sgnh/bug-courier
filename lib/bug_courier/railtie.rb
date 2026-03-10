2199fae7a58acdf97d64263c527310c7582c0b77
Steffen Hansen <sgnh@users.noreply.github.com>
2026-03-10 13:26:48 +0100

    Update bug_courier to version 0.1.2
    Bump the bug_courier gem version to 0.1.2 and adjust middleware
    insertion order in the Railtie to ensure proper functionality.

--- lib/bug_courier/railtie.rb
+++ lib/bug_courier/railtie.rb
@@ -5,7 +5,7 @@ require "rails/railtie"
 module BugCourier
   class Railtie < Rails::Railtie
     initializer "bug_courier.configure_middleware" do |app|
-      app.middleware.insert_before(0, BugCourier::Middleware)
+      app.middleware.insert_after(ActionDispatch::DebugExceptions, BugCourier::Middleware)
     end
 
     initializer "bug_courier.set_logger" do
--- lib/bug_courier/version.rb
+++ lib/bug_courier/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module BugCourier
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
